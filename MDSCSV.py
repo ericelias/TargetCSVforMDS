751a793f665e2fb377e17e3f75280e34c9145b12
ericelias <110695507+ericelias@users.noreply.github.com>
2022-08-06 15:28:00 -0700

    Update MDSCSV.py

--- MDSCSV.py
+++ MDSCSV.py
@@ -1,24 +1,31 @@
 def mdscsv():
 
-	from deepface import DeepFace					### All the modules needed
+	from deepface import DeepFace			### All the modules needed
 	import matplotlib.pyplot as plt
 	import pandas as pd
 	import csv
-
+	from striprtf.striprtf import rtf_to_text
+	
 	models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib", "SFace"]
-	modelname = models[1]						### Facial recognition program to use. Type the number from the list above 
-									### that you want to use into models[]
+	modelname = models[4]		### Facial recognition program to use. Type the number from the list above 
+					### that you want to use into models[]
 
-	csvloc = "" 							### Place csv path into the quotes
-									### A new .csv will be created if there isn't one in the path
+	csvloc = "" 			### Place csv path into the quotes
+					### A new .csv will be created if there isn't one in the path
 
-	### Input target paths in quotes separated by commas into list (can be any amount of targets)
-	imglist = []
+	### Input rtf path that contains paths of target images
+	textfilertf = ""
+
+
+	with open(textfilertf) as infile:
+    	content = infile.read()
+    	text = rtf_to_text(content)
+	newlist = [text]
+	imglist = newlist[0].split()
 
 	targets = [] 							### Creates list with abbreviated pathnames of targets to use in csv file
 	for z in imglist:								
-	    targets.append(z[-20:-4])					### You'll have to adjust append.z[] based on your 
-									### filenames for a clean looking output
+	    targets.append(z[-14:-4])					### You'll have to adjust append.z[] based on your filenames
 
 	header = [""] 							### Creates header list from targets to be analyzed
 	for i in range(len(targets)):
@@ -37,7 +44,6 @@ def mdscsv():
 			newrow.append(resp['distance'])
 		with open(csvloc, "a") as f:
 			data = csv.writer(f)
-			data.writerow(newrow)
-			
+			data.writerow(newrow)	
 	f.close()
 mdscsv()
